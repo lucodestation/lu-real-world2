7cc07b8d56e5791df8b074d770f82f05f50d5f57
卢忠伟 <lucodestation@163.com>
2021-11-12 12:31:18 +0800

    修改点儿备注

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 // const requestBaseUrl = 'https://lu-real-world.vercel.app/api';
 const requestBaseUrl = 'https://cirosantilli-realworld-next.herokuapp.com/api';
-// https://cirosantilli-realworld-next.herokuapp.com/api
 
 const tokenPrefix = 'Token ';
 
